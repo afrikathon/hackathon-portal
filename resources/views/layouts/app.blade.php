e2d6ee74ffd71db1d23388c0e60ccd112b19990e
eadortsu <mail@eadortsu.com>
2020-04-21 13:59:38 +0000

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,9 @@ Rich in diversity and culture, let’s tap into our innermost potential to creat
     @include('layouts.page_templates.auth')
 @endauth
 
+@guest
+    @include('layouts.page_templates.guest')
+@endguest
 
 <!--   Core JS Files   -->
 <script src="{{ asset('paper') }}/js/core/jquery.min.js"></script>
